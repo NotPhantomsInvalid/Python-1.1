d1c9ec5b25297d417585fb6e5ee48721a653355a
Fernando Martinez <psicodelicos.fer@gmail.com>
2024-12-26 10:50:21 -0600

    commit 2

--- aprender.py
+++ aprender.py
@@ -1,2 +1,34 @@
-#Aprendamos python juntos
+#Aprendamos python juntos. Empezaremos con el clasico "Hola mundo" pero esta vez ocuparemos "Hola raton sin cola"
+print("_________________")
 print("Hola raton sin cola")
+
+#_____________________
+#Declaracion de variables
+x='Fernando'
+print(x)
+print("_________________")
+#Sumar variables
+a=10
+b=5
+print(a+b)
+print(a/b)
+print(a*b)
+print("_________________")
+c=a-b
+print(a-b)
+print("_________________")
+#Ejemplo de condicional
+if c<a:
+    print('El resultado de la resta',a,'y',b,'es menor que ',a)
+if not c==5:
+    print('Tu resultado es ',c)
+    
+
+
+
+
+
+
+
+
+
