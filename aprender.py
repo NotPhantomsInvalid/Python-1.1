6d947af4ad920e8cb2d20bb4bdcacb96dc640beb
Fernando Martinez <psicodelicos.fer@gmail.com>
2024-12-26 19:42:21 -0600

    commit 3

--- aprender.py
+++ aprender.py
@@ -1,31 +1,26 @@
-#Aprendamos python juntos. Empezaremos con el clasico "Hola mundo" pero esta vez ocuparemos "Hola raton sin cola"
-print("_________________")
+#Imprimir hola mundo pero a mi manera.RIP Mtro. Moronati0
 print("Hola raton sin cola")
-
-#_____________________
-#Declaracion de variables
-x='Fernando'
+print('Hola raton sin cola')
+print('''hola raton sin cola''')
+#Declaracion de variables.
+x= 'Fernando'
 print(x)
-print("_________________")
-#Sumar variables
-a=10
-b=5
-print(a+b)
-print(a/b)
-print(a*b)
-print("_________________")
-c=a-b
+#Operaciones aritmeticas y suma de variables.
+a= 10
+b= 20
+c= a+b
+print(c)
 print(a-b)
-print("_________________")
-#Ejemplo de condicional
-if c<a:
-    print('El resultado de la resta',a,'y',b,'es menor que ',a)
-if not c==5:
-    print('Tu resultado es ',c)
-    
-
-
-
+print(a*b)
+#Condicionales
+if c>a:
+    print(c,' es mayor que',a)
+else:
+    print(a,' es menor que',c)
+#Datos en decimales
+dato_decimal= 3.1416
+cade_texto= 'El valor de pi es: '
+print(cade_texto,dato_decimal)
 
 
 
