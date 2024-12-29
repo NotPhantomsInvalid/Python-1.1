a33ff12403ad9ede06bf6adaddbbd7030fbb22a1
Fernando Martinez <psicodelicos.fer@gmail.com>
2024-12-29 00:36:15 -0600

    commitv2

--- aprender2.py
+++ aprender2.py
@@ -4,3 +4,16 @@ Variable_b= 'Fernando'
 Variable_c= 3.1415
 Variable1=20
 VariaBle2=30
+
+#Esto esta interesante, podemos asignar múltiples variables en una liena de código
+x,y,z = 1,2,3
+
+#Ahora haremos un poco de condicionales (if, elif, else)
+code="Python"
+if code == "Python":
+    print("Python es el mejor lenguaje de programación, no tiren hate por fa ")
+elif code == "Java":
+    print("No me mola el M I N E C R A F T JAVA ")
+else:
+    print ("Python your ck is the b e s t ")
+
